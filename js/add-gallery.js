70801c3e1127ea58375a6425c08ad0aeda660a5a
playsoksana <playsoksana@gmail.com>
2021-04-12 16:54:42 +0300

    open/close modal

--- js/add-gallery.js
+++ js/add-gallery.js
@@ -4,7 +4,7 @@ import ulGallery from './gallery-items.js';
 const body = document.querySelector('body');
 const ulEl = document.createElement('ul');
 ulEl.classList.add('gallery');
-
+//makeGallery
 const makeGallery = (array) => {
 
   const result = array.map(({ preview, original, description }) =>
@@ -20,16 +20,53 @@ const makeGallery = (array) => {
   ulEl.innerHTML = result.join('');
   return ulEl;
 };
-
 makeGallery(ulGallery);
-body.prepend(ulEl);
+
+//makeModal
+
+  const lightboxEl = document.createElement('div')
+  lightboxEl.classList.add('lightbox');
+
+  const lightboxOverlayEl = document.createElement('div');
+  lightboxOverlayEl.classList.add('lightbox__overlay');
+
+const lightboxContentEl = document.createElement('div');
+lightboxContentEl.classList.add('lightbox__content')
+
+const lightboxImageEl = document.createElement('img');
+lightboxImageEl.classList.add('lightbox__image');
+
+const lightboxButtonEl = document.createElement('button');
+lightboxButtonEl.classList.add('lightbox__button');
+lightboxButtonEl.setAttribute('type', 'button');
+lightboxContentEl.appendChild(lightboxImageEl);
+lightboxOverlayEl.append(lightboxContentEl, lightboxButtonEl);
+lightboxEl.appendChild(lightboxOverlayEl);
+
+
+body.prepend(ulEl, lightboxEl);
 
 const photoEl = document.querySelector('.gallery__item');
 //Делигирование 
 function clickOnGallery(event) {
+  event.preventDefault();
   if (!event.target.classList.contains('gallery__link')) {
-    return
+    console.log('мимо');
   }
-  console.log('фото');
-}
-body.addEventListener('click', clickOnGallery);
+  //открітие модалки
+ lightboxEl.classList.add('is-open');
+ lightboxEl.querySelector('.lightbox__image').src = event.target.dataset.source;
+ lightboxEl.querySelector('.lightbox__image').alt = event.target.alt;
+console.log(lightboxEl);
+
+};
+ulEl.addEventListener('click', clickOnGallery);
+
+
+//закрытие модалки 
+const btnCloseEl = document.querySelector('.lightbox__button');
+function closeBtn () {
+  lightboxEl.classList.remove('is-open');
+};
+
+btnCloseEl.addEventListener('click', closeBtn);
