6597e70d405a36a7eb5585d1658d7e85263a38c9
playsoksana <playsoksana@gmail.com>
2021-04-13 17:04:39 +0300

    111

--- js/add-gallery.js
+++ js/add-gallery.js
@@ -2,10 +2,12 @@ import ulGallery from './gallery-items.js';
 
 // Создание разметки 
 const listGalleryEl = document.querySelector('.js-gallery');
+
 const lightboxButtonEl = document.querySelector('button[data-action="close-lightbox"]');
 const lightboxEl = document.querySelector('.js-lightbox');
 const lightboxImageEl = lightboxEl.querySelector('.lightbox__image');
-const lightboxOverlayEl = lightboxEl.querySelector('.lightbox__overlay')
+const lightboxOverlayEl = lightboxEl.querySelector('.lightbox__overlay');
+
 
 //makeGallery
 const makeGallery = (array) => {
@@ -26,7 +28,17 @@ makeGallery(ulGallery);
 //Делигирование 
 listGalleryEl.addEventListener('click', openGallery);
 
+lightboxEl.removeEventListener('click', closeModal);
+window.removeEventListener('keydown', closeModal);
+lightboxButtonEl.removeEventListener('click', closeModal);
+
+
 function openGallery(event) {
+  listGalleryEl.removeEventListener('click', openGallery);
+lightboxEl.addEventListener('click', closeModal);
+window.addEventListener('keydown', closeModal);
+lightboxButtonEl.addEventListener('click', closeModal);
+
   event.preventDefault();
   if (!event.target.classList.contains('gallery__image')) {
     return;
@@ -37,17 +49,19 @@ function openGallery(event) {
 };
 
 function addAttributeOnLightboxImage(event) {
-  lightboxEl.querySelector('.lightbox__image').src = event.target.dataset.source;
-  lightboxEl.querySelector('.lightbox__image').alt = event.target.alt;
+  lightboxImageEl.src = event.target.dataset.source;
+  lightboxImageEl.alt = event.target.alt;
   return;
 };
 
 //закрытие модалки 
-lightboxButtonEl.addEventListener('click', closeModal);
+
 
 
 function closeModal(event) {
+  
   if (event.target.nodeName === 'BUTTON' || event.code === 'Escape' || event.target === lightboxOverlayEl) {
+    listGalleryEl.addEventListener('click', openGallery);
     lightboxEl.classList.remove('is-open');
     lightboxImageEl.removeAttribute('src');
     lightboxImageEl.removeAttribute('alt');
@@ -56,21 +70,28 @@ function closeModal(event) {
   return;
 };
 
-lightboxEl.addEventListener('click', closeModal);
-window.addEventListener('keydown', closeModal);
 
-window.addEventListener('keyup', aaa);
-function aaa(event) {
-  console.log(event.code);
-  if (event.code === 'ArrowRight') {
-    console.log('ArrowRight');
-
-    return;
-  };
-  if (event.code === 'ArrowLeft') {
-    console.log('ArrowLeft');
-
-    return;
-  };
-  return;
+const arrImgPreview = listGalleryEl.querySelectorAll('.gallery__image');
+console.log(arrImgPreview);
+arrImgPreview.forEach(img => img.addEventListener('load', print));
+function print (event) {
+  console.log('LOAD')
 };
+/////////////////////////////////////////////////////
+
+
+
+// window.addEventListener('keyup', flickThrough);
+// function flickThrough(event) {
+//   console.log(event.code);
+//   if (event.code === 'ArrowRight') {
+//             console.log('111');
+//     return;
+//   };
+//   if (event.code === 'ArrowLeft') {
+//     console.log('ArrowLeft');
+
+//     return;
+//   };
+//   return;
+// };
